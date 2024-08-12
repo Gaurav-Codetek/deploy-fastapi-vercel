489db888b49ad0bd5a7dd16e77c35271a3a6a80a
Gaurav Patel <gp43883@gmail.com>
2024-08-13 01:04:44 +0530

    added cors

--- app/app.py
+++ app/app.py
@@ -2,10 +2,22 @@ from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from pymongo import MongoClient
 from dotenv import load_dotenv
+from fastapi.middleware.cors import CORSMiddleware
 import os
 load_dotenv()
 app = FastAPI()
 
+origins = [
+    "http://localhost:3000"  # Replace with your actual frontend domain
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 # Define the request body using Pydantic model
 class PromptRequest(BaseModel):
     prompt: str
